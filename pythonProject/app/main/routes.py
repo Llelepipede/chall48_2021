6fd5ff7e2a0e1ea7595f945be26ccbf2704c6398
root <root@MSI.localdomain>
2021-02-23 11:32:14 +0100

    ajout image fonctionnel

--- pythonProject/app/main/routes.py
+++ pythonProject/app/main/routes.py
@@ -1,4 +1,6 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, redirect, url_for, request
+
+from app.extensions import mongo
 
 main = Blueprint('main', __name__)
 
@@ -6,3 +8,17 @@ main = Blueprint('main', __name__)
 @main.route('/')
 def index():
     return render_template("index.html")
+
+
+@main.route('/ajout_images', methods=['POST'])
+def ajout_images():
+    if 'image' in request.files:
+        print("ok")
+        image = request.files['image']
+        mongo.save_file(image.filename, image)
+        mongo.db.equipe40.insert_one({
+            'image_name': image.filename
+
+        })
+
+    return redirect(url_for('main.index'))
